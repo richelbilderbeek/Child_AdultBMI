a1d62f0a6bcd0b3e4c68df47f97d94194ea55410
eleanorsanderson <eleanor.sanderson@bristol.ac.uk>
2020-05-04 16:42:42 +0100

    simulation results

--- combinefiles.R
+++ combinefiles.R
@@ -1,19 +1,16 @@
 
 rm(list = ls(all=TRUE))
 
-load("sim_output_A1.Rda")
-resultsA_all <- resultsA
-load("sim_output_B1.Rda")
-resultsB_all <- resultsB
-load("sim_output_C1.Rda")
-resultsC_all <- resultsC
+resultsA_all <- NULL
+resultsB_all <- NULL
+resultsC_all <- NULL
 
 
 file.namesA <- dir(pattern ="sim_output_A")
 file.namesB <- dir(pattern ="sim_output_B")
 file.namesC <- dir(pattern ="sim_output_C")
 
-for (i in 2:40){
+for (i in 1:40){
   
   load(file.namesA[i])
   load(file.namesB[i])
@@ -26,5 +23,5 @@ for (i in 2:40){
 }
 
 save(resultsA_all, file=("Results_A.Rda"))
-save(resultsB_all, file=("Tesults_B.Rda"))
+save(resultsB_all, file=("Results_B.Rda"))
 save(resultsC_all, file=("Results_C.Rda"))

a009363e074627634030883eba97a4548ce06d35
eleanorsanderson <eleanor.sanderson@bristol.ac.uk>
2020-05-04 16:34:46 +0100

    update to combining files

--- combinefiles.R
+++ combinefiles.R
@@ -1,7 +1,6 @@
 
 rm(list = ls(all=TRUE))
 
-
 load("sim_output_A1.Rda")
 resultsA_all <- resultsA
 load("sim_output_B1.Rda")
@@ -9,10 +8,16 @@ resultsB_all <- resultsB
 load("sim_output_C1.Rda")
 resultsC_all <- resultsC
 
-for (i in 1:40){
-  load("sim_output_A1.Rda")
-  load("sim_output_B1.Rda")
-  load("sim_output_C1.Rda")
+
+file.namesA <- dir(pattern ="sim_output_A")
+file.namesB <- dir(pattern ="sim_output_B")
+file.namesC <- dir(pattern ="sim_output_C")
+
+for (i in 2:40){
+  
+  load(file.namesA[i])
+  load(file.namesB[i])
+  load(file.namesC[i])
   
   resultsA_all <- rbind(resultsA_all, resultsA)
   resultsB_all <- rbind(resultsB_all, resultsB)
@@ -20,6 +25,6 @@ for (i in 1:40){
   
 }
 
-save(resultsA_all, file=("sim_output_A.Rda"))
-save(resultsB_all, file=("sim_output_B.Rda"))
-save(resultsC_all, file=("sim_output_C.Rda"))
+save(resultsA_all, file=("Results_A.Rda"))
+save(resultsB_all, file=("Tesults_B.Rda"))
+save(resultsC_all, file=("Results_C.Rda"))
